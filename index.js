d192c6ae36d06e8ee20ec0197410b73029d95212
PrathamSakhare <psakhre@themohh.com>
2024-01-06 22:17:27 +0530

    settled up the basic server using express js and tested on api / @prathamsakhare

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-console.log('Hello World!');
+// making up server - initialising express
 
+const express = require('express');
+const server = express();
+const PORT = 8080;
 
+server.get('/', (req, res) => {
+    res.json({status : 'success'});
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`);
+})
 
 
 
