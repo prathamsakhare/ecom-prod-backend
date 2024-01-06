e55eb06d9fc46d32ef7f9934cfe110e70a422708
PrathamSakhare <psakhre@themohh.com>
2024-01-06 22:29:31 +0530

    connect it to actual mongodb, import mongoose, make main function, make dir 'data', go to terminal, type 'mongod --dbpath ./data', and run the main function @prathamsakhare

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const express = require('express');
 const server = express();
 const PORT = 8080;
 
+// connection with mongoose
+const mongoose = require("mongoose");
+// ! after doing this make connection with mongodb, for that, go to your project directory, outside your project directory, make new dir named as "data", after that, go to one step above that data directory, and run this command "mongod --dbpath=./data", this will start your mongodb server, now you can connect with mongodb
+
+
+main().catch(err => console.log(err));
+
+async function main(){
+    await mongoose.connect('mongodb://localhost:27017/ecommerce');
+    console.log('Connected to MongoDB...');
+}
+
 server.get('/', (req, res) => {
     res.json({status : 'success'});
 });
