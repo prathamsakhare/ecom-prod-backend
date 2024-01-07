475c0f761c9498b5c67d1fffc081f3fead7f44e8
PrathamSakhare <psakhre@themohh.com>
2024-01-07 13:24:47 +0530

    corrected some errors in category and brand filtering, also exposed headers of x-total-count to cors so that pagination can be used by client @prathamsakhare

--- controller/Product.js
+++ controller/Product.js
@@ -14,48 +14,95 @@ exports.createProduct = (req, res) => {
 };
 
 // Fetching all products
+// exports.fetchAllProducts = async (req, res) => {
+//   let query = Product.find();
+//   let totalProductsQuery = Product.find();
+
+//   // * query for category and brand
+//   if ((req, query.category)) {
+//     query = query.find({ category: req.query.category });
+//     totalProductsQuery = (await totalProductsQuery).findIndex({
+//       category: req.query.category,
+//     });
+//   }
+//   if (req.query.brand) {
+//     query = query.find({ brand: req.query.brand });
+//     totalProductsQuery = (await totalProductsQuery).findIndex({
+//       brand: req.query.brand,
+//     });
+//   }
+
+//   // * query for sorting (Ascending or Descending Order)
+//   if (req.query._sort && req.query._order) {
+//     query = query.sort([[req.query._sort, req.query._order]]);
+//   }
+
+//   // * query for finding total count of documents
+//   const totalDocs = await totalProductsQuery.count().exec();
+//   console.log({ totalDocs });
+
+//   // * query for pagination
+//   if (req.query._page && req.query._limit) {
+//     const pageSize = req.query._limit;
+//     const page = req.query._page;
+//     query = query.skip(pageSize * (page - 1)).limit(pageSize);
+//   }
+
+//   try {
+//     const docs = await query.exec();
+//     res.set("X-Total-Count", totalDocs);
+//     res.status(200).json(docs);
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// };
 exports.fetchAllProducts = async (req, res) => {
-  let query = Product.find();
-  let totalProductsQuery = Product.find();
+    // filter = {"category":["smartphone","laptops"]}
+    // sort = {_sort:"price",_order="desc"}
+    // pagination = {_page:1,_limit=10}
+    let condition = {}
+    if(!req.query.admin){
+        condition.deleted = {$ne:true}
+    }
+    
+    let query = Product.find(condition);
+    let totalProductsQuery = Product.find(condition);
+  
+    console.log(req.query.category);
+  
+    if (req.query.category) {
+      query = query.find({ category: {$in:req.query.category.split(',')} });
+      totalProductsQuery = totalProductsQuery.find({
+        category: {$in:req.query.category.split(',')},
+      });
+    }
+    if (req.query.brand) {
+      query = query.find({ brand: {$in:req.query.brand.split(',')} });
+      totalProductsQuery = totalProductsQuery.find({ brand: {$in:req.query.brand.split(',') }});
+    }
 
-  // * query for category and brand
-  if ((req, query.category)) {
-    query = query.find({ category: req.query.category });
-    totalProductsQuery = (await totalProductsQuery).findIndex({
-      category: req.query.category,
-    });
-  }
-  if (req.query.brand) {
-    query = query.find({ brand: req.query.brand });
-    totalProductsQuery = (await totalProductsQuery).findIndex({
-      brand: req.query.brand,
-    });
-  }
-
-  // * query for sorting (Ascending or Descending Order)
-  if (req.query._sort && req.query._order) {
-    query = query.sort([[req.query._sort, req.query._order]]);
-  }
-
-  // * query for finding total count of documents
-  const totalDocs = await totalProductsQuery.count().exec();
-  console.log({ totalDocs });
-
-  // * query for pagination
-  if (req.query._page && req.query._limit) {
-    const pageSize = req.query._limit;
-    const page = req.query._page;
-    query = query.skip(pageSize * (page - 1)).limit(pageSize);
-  }
-
-  try {
-    const docs = await query.exec();
-    res.set("X-Total-Count", totalDocs);
-    res.status(200).json(docs);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-};
+    // TODO : How to sort the products based on discounted prices rather than original one
+    if (req.query._sort && req.query._order) {
+      query = query.sort({ [req.query._sort]: req.query._order });
+    }
+  
+    const totalDocs = await totalProductsQuery.count().exec();
+    console.log({ totalDocs });
+  
+    if (req.query._page && req.query._limit) {
+      const pageSize = req.query._limit;
+      const page = req.query._page;
+      query = query.skip(pageSize * (page - 1)).limit(pageSize);
+    }
+  
+    try {
+      const docs = await query.exec();
+      res.set('X-Total-Count', totalDocs);
+      res.status(200).json(docs);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  };
 
 exports.fetchProductById = async (req, res) => {
   const { id } = req.params;
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const server = express();
 const PORT = 8080;
 
+// * cors policy
+const cors = require('cors');
+
 //Routes 
 const productRouter = require('./routes/Product');
 const brandRouter = require('./routes/Brand');
@@ -15,6 +18,10 @@ const { createProduct } = require('./controller/Product');
 // ! after doing this make connection with mongodb, for that, go to your project directory, outside your project directory, make new dir named as "data", after that, go to one step above that data directory, and run this command "mongod --dbpath=./data", this will start your mongodb server, now you can connect with mongodb
 
 // * Middleware
+server.use(cors({
+    // * you have to expose the headers to the client, otherwise, you will get error, for the pagination, the total count should be exposed to the client, if you dont expose it, the pagination will not get shown to client
+    exposedHeaders : ['X-Total-Count']
+}));
 server.use(express.json()); // this will parse the json data from the body of the request
 
 // * Routes
